0cb3d43b7644eabe32770a9049bed3f8703d8359
Stephan Wentz <stephan@wentz.it>
2013-05-15 13:57:06 +0200

    import

---
+++ src/Brainbits/FugueIconsBundle/BrainbitsFugueIconsBundle.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the brainbits fugue icons bundle.
+ *
+ * For the full copyright and license information, please view the README.txt
+ * file that was distributed with this source code.
+ */
+
+namespace Brainbits\FugueIconsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * brainbits fugue icons bundle
+ *
+ * @author Stephan Wentz <swentt@brainbits.net>
+ */
+class BrainbitsFugueIconsBundle extends Bundle
+{
+}
